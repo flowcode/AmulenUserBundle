c0bfb3b7beb466153f3825b48ce9bc51b5d9ecec
juliansci <julian.scialabba@gmail.com>
2017-02-02 16:32:05 -0300

    Api con version en url no query string

--- src/Flowcode/UserBundle/Controller/Api/SecurityController.php
+++ src/Flowcode/UserBundle/Controller/Api/SecurityController.php
@@ -8,14 +8,8 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\Controller\Annotations\Version;
 
-/**
- * @Version("v1")
- */
 class SecurityController extends FOSRestController
 {
-    /**
-     * @Post("/login", name="api_security_login",  options={ "method_prefix" = false })
-     */
     public function loginAction()
     {
         return 'login v1';
